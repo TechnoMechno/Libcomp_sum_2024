bea38250f25d3a4cd412f423887d165c6d32bdfd
technomechno <davidpro1080@gmail.com>
2024-07-13 14:54:34 -0700

    gigity

--- python_test.py
+++ python_test.py
@@ -1,6 +1,21 @@
 import subprocess
+from python_tkinterMini.tkinterMini import SimpleGUI
 
-result = subprocess.run(['./test'], text=True, capture_output=True)
-print(result.stdout)
+n = 0
+
+# create the app instance
+app = SimpleGUI(title="Tkinter Example", width=400, height=300)
+app.add_label("This is a Joke Generator!", row=0, column=0)
+
+app.add_label("", row=3, column=0)
 
 
+def on_button_click():
+    global n
+    result = subprocess.run(['./jokeWriter'], text=True, capture_output=True)
+    
+    app.add_label(result.stdout, row=3, column=0)
+    
+app.add_button("Click Me", command=on_button_click, row=1, column=0)
+
+app.run()
